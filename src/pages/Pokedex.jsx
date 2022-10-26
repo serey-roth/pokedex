46ab56455898f2907bd1e11668729dc48206eb76
serey-roth <sereyratanakroth@gmail.com>
2022-10-26 12:13:27 -0700

    get pokemon entries based on pokedex region

--- src/pages/Pokedex.jsx
+++ src/pages/Pokedex.jsx
@@ -5,9 +5,11 @@ import Loader from '../components/Loader';
 import Error from '../components/Error';
 import PokemonCard from '../components/PokemonCard';
 import SearchBar from '../components/SearchBar';
+import PokedexRegions from '../components/PokedexRegions';
 
-import { useGetPokemonsQuery } from '../redux/services/pokemonApi';
+import { useGetPokedexQuery, useGetPokemonsQuery } from '../redux/services/pokemonApi';
 import { updatePokedex } from '../redux/features/pokedexSlice';
+
 
 const Pokedex = ({ pageNum, setPageNum }) => {
     const dispatch = useDispatch();
@@ -18,17 +20,17 @@ const Pokedex = ({ pageNum, setPageNum }) => {
     const [search, updateSearch] = useState('');
     //the results from the search
     const [searchResults, updateSearchResults] = useState(null);
-    //the list of all pokemon names
-    const pokedex = useSelector(state => state.pokedex);
+    const [activeRegionSelect, setActiveRegionSelect] = useState(true);
 
-    //get the names of all pokemons
-    const { data, isFetching, error } = useGetPokemonsQuery(pokedex.length > 0 ? -1 : 1000); 
+    //the list of all pokemon names
+    const { list: pokedex, region } = useSelector(state => state.pokedex);
+
+    const { data, isFetching, error} = useGetPokedexQuery(region);
 
     useEffect(() => {
-        if (data?.results) {
-            const entries = data.results.map(result => 
-                /[a-z]+\-[a-z]{2,}/g.test(result.name) ? null : result.name)
-                .filter(entry => entry !== null);
+        if (data) {
+            const entries = data.pokemon_entries?.map(entry =>
+                entry.pokemon_species?.name);
             dispatch(updatePokedex(entries));
         }
     }, [data]);
@@ -36,7 +38,7 @@ const Pokedex = ({ pageNum, setPageNum }) => {
     useEffect(() => {
         //the numbers displayed depends on the page number which depends
         //on how far the user has scrolled down the page
-        const count = 8 + 4 * pageNum;
+        const count = 15 + 5 * pageNum;
         //if there is a search, then show the search results based on the 
         //given count; otherwise, show the pokedex based on the count
         if (searchResults) updateNamesSoFar(searchResults.slice(0, count));
@@ -47,9 +49,11 @@ const Pokedex = ({ pageNum, setPageNum }) => {
         if (search && pokedex.length > 0) {
             const results = pokedex.filter(name => name.includes(search));
             updateSearchResults(results);
+            setActiveRegionSelect(false)
         }
         if (!search) {
             updateSearchResults(null);
+            setActiveRegionSelect(true);
         }
         setPageNum(0);
         //scroll to top of the pokedex for every new search or no search
@@ -61,17 +65,19 @@ const Pokedex = ({ pageNum, setPageNum }) => {
     if (error) return (<Error />);
 
     return (
-        <div className='w-full flex flex-col items-start relative'>
-            <div className='w-full flex flex-wrap fixed z-10
-            items-center justify-center gap-3 bg-black text-yellow-400 p-3'>
+        <div className='w-full flex flex-col items-start relative min-h-screen'>
+            <div className='w-full flex flex-wrap items-center fixed z-10
+            justify-center bg-black text-white p-3 gap-3 lg:h-[75px]
+            h-fit'>
+                <PokedexRegions active={activeRegionSelect} />
                 <h1 className='animate-slideleft
                 font-bold uppercase text-2xl'>
                     Pokedex
                 </h1>    
                 <SearchBar search={search} updateSearch={updateSearch} />
             </div>
-            <div className='flex flex-col sm:grid mt-[4.5em]
-            sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 w-full'>
+            <div className='flex items-center justify-center mt-[4.5em]
+            flex-wrap w-full'>
                 {namesSoFar.map(name => 
                 (<PokemonCard key={`pokemon-${name}`} query={name} />))}
             </div>
