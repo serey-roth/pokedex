aa813dc59ee6aadae506ea072af18bcbd8717576
Serey Roth <sereyratanakroth@gmail.com>
2022-12-08 15:48:35 -0800

    moved handler up to parent and removed prop

--- src/components/PokedexRegions.jsx
+++ src/components/PokedexRegions.jsx
@@ -1,7 +1,3 @@
-import React, { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import { updateRegion } from '../redux/features/pokedexSlice';
-
 const regions = ['national', 'kanto',
     'original-johto', 'updated-johto', 'hoenn', 'updated-hoenn',
     'original-sinnoh', 'extended-sinnoh', 'original-unova', 'updated-unova',
@@ -12,20 +8,12 @@ const regions = ['national', 'kanto',
     'original-poni', 'updated-poni',
     'galar', 'isle-of-armor', 'crown-tundra', 'hisui'];
 
-const PokedexRegions = ({ active }) => {
-    const dispatch = useDispatch();
-    const currentRegion = useSelector(state => state.pokedex.region);
-
-    const handleChange = (e) => {
-        dispatch(updateRegion(e.target.value.replace(/\'/g, '').replace(/\s/g, '')));
-    }
-
+const PokedexRegions = ({ region, onChange }) => {
     return (
         <select className={`text-black font-semibold
         text-sm text-center p-2 appearance-none cursor-pointer
-        rounded-lg capitalize outline-none 
-        ${!active && 'pointer-events-none opacity-50'}`}
-        value={currentRegion} onChange={handleChange}>
+        rounded-lg capitalize outline-none`}
+        value={region} onChange={ onChange }>
             {regions.map(region => (
                 <option
                 key={region} 
