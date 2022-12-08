f6057790f7be8d49e971c7b8c4fba874161ff038
Serey Roth <sereyratanakroth@gmail.com>
2022-12-08 15:47:38 -0800

    removed modals and added providers for app

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
-import { Routes, Route } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import Pokedex from './pages/Pokedex';
 import Pokemon from './pages/Pokemon';
-import MoveModal from './components/modal/MoveModal';
-import AbilityModal from './components/modal/AbilityModal';
+
+const queryClient = new QueryClient();
 
 const App = () => {
     return (
         <div className='flex flex-col bg-white/50 w-screen min-h-screen relative'>
-            <Routes>
-                <Route path='/' element={<Pokedex />} />
-                <Route path='/pokemon/:id' element={<Pokemon />} />
-            </Routes>
-            <MoveModal />
-            <AbilityModal />
+            <QueryClientProvider client={queryClient}>
+                <Router>
+                    <Routes>
+                        <Route path='/' element={<Pokedex />} />
+                        <Route path='/pokemon/:id' element={<Pokemon />} />
+                    </Routes>
+                </Router>
+            </QueryClientProvider>
         </div>
     )
 }
