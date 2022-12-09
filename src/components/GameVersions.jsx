c93cb65f28d950f2c69e859136f8328b7560ecce
Serey Roth <sereyratanakroth@gmail.com>
2022-12-09 15:20:51 -0800

    removed props and redux elements

--- src/components/GameVersions.jsx
+++ src/components/GameVersions.jsx
@@ -1,10 +1,7 @@
 import React from 'react'
-import { useSelector, useDispatch } from 'react-redux';
 
 import { types } from '../assets';
-
-import { setSelectGeneration, setVersion } from '../redux/features/pokemonSlice';
-
+import { usePokemonContext } from '../features/pokemonContext';
 const versions = {
     'red/blue': 'red-blue',
     'yellow': 'yellow' ,
@@ -26,11 +23,20 @@ const versions = {
     'sword/shield': 'sword-shield',
 }
 
-const GameVersions = ({ version, onChange }) => {
+const GameVersions = () => {
+    const { selections, handleSelectionsChange } = usePokemonContext();
+    
+    const handleVersionChange = (e) => {
+        handleSelectionsChange({
+            name: 'version',
+            value: e.target.value,
+        });
+    }
+
     return (
         <select className={`rounded-lg p-2 uppercase bg-white bg-opacity-80
         font-bold outline-none border-2`}
-        value={version}
+        value={selections.version}
         onChange={onChange}>
         {Object.keys(versions).map(gameVersion => (
             <option key={`version-${gameVersion}`}
