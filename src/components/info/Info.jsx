7f848bd22846fa14799328f0a1e9c4d054f4fa33
Serey Roth <sereyratanakroth@gmail.com>
2022-12-08 21:47:20 -0800

    removed redux elements

--- src/components/info/Info.jsx
+++ src/components/info/Info.jsx
@@ -1,20 +1,14 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
 
 import BaseInfo from './BaseInfo'
 import Breeding from './Breeding'
 import Training from './Training'
 
-import { types } from '../../assets'
-
 const Info = () => {
-    const type = useSelector(state => state.pokemon.type);
-
     return (
         <div className='flex flex-col
         items-center justify-center w-full'>
             <h1 className={`font-bold text-xl uppercase
-            ${type && types[type].backgroundColor} text-white
             rounded-lg p-2`}>
                 Pokemon Data
             </h1> 
