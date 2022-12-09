51e107d82310220918aa97314dacbf3d391580f3
Serey Roth <sereyratanakroth@gmail.com>
2022-12-08 18:37:56 -0800

    replace Loader and Error with skeletons

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -26,46 +26,40 @@ const PokemonCard = ({ query }) => {
         }
     }
 
-    if (isLoading && isFetching) 
-    return (
-        <PokemonPlaceHolder>
-            <ImSpinner className='w-[50px] h-[50px] animate-spin' />
-        </PokemonPlaceHolder>
-    )
-
-    if (isError && error) 
-    return (
-        <PokemonPlaceHolder>
-            Error!
-        </PokemonPlaceHolder>
-    )
-
     if (!data?.is_default) return null;
 
     return (
         <div className='flex flex-col h-fit p-2 border-[0.5px] relative'>
-            <span className='self-center'>
-                <LoadedImage 
-                    width={80} 
-                    height={80} 
-                    name={data?.name}
-                    src={data?.sprites?.other['official-artwork'].front_default} />
-            </span>
-
-            <span className='flex flex-wrap items-center gap-1'>
-                <h3 className={`font-[700] text-sm
-                ${data && types[data.types[0].type.name].textColor}`}>
-                    {data?.id}
-                </h3>
-                <h1 
-                    className={`flex-1 capitalize text-sm truncate
-                    cursor-pointer text-right ${data && 
-                    types[data.types[0].type.name].textColor}`}
-                    onClick={() => handleClick(data?.name)}
-                >
-                    {data?.name?.replace(/\-[a-z]{3,}$/g, '')}
-                </h1>
-            </span>
+            {(isLoading || isFetching || (isError && error)) ? (
+                <>
+                    <span className='w-[80] h-[80] bg-slate-50 animate-pulse' />
+                    <span className='w-full h-[10] bg-slate-50 animate-pulse' />
+                </>
+            ) : (
+                <>
+                    <span className='self-center'>
+                        <LoadedImage 
+                            width={80} 
+                            height={80} 
+                            name={data?.name}
+                            src={data?.sprites?.other['official-artwork'].front_default} />
+                    </span>
+                    <span className='flex flex-wrap items-center gap-1'>
+                        {<h3 className={`font-[700] text-sm
+                        ${data && types[data.types[0].type.name].textColor}`}>
+                            {data?.id}
+                        </h3>}
+                        <h1 
+                            className={`flex-1 capitalize text-sm truncate
+                            cursor-pointer text-right ${data && 
+                            types[data.types[0].type.name].textColor}`}
+                            onClick={() => handleClick(data?.name)}
+                        >
+                            {data?.name?.replace(/\-[a-z]{3,}$/g, '')}
+                        </h1>
+                    </span>
+                </>
+            )}
         </div>
     )
 }
